2e9b2e0553dd45d4af6b07d520d2fd8ee7683820
FentonsIT <github@fentonsit.com>
2023-09-14 17:28:46 +0100

    Revert "Update DomainsAnalyser.js"
    This reverts commit 93bcb99f41ef466709aedc036e2645b30039da7f.

--- src/views/tenant/standards/DomainsAnalyser.js
+++ src/views/tenant/standards/DomainsAnalyser.js
@@ -275,7 +275,8 @@ const DomainsAnalyser = () => {
       showAllTenantSelector={true}
       datatable={{
         filterlist:[
-          { filterName: 'Exclude onmicrosoft.com', filter: '"IsInitial":false' },
+          {filterName: 'IsInitial onmicrosoft.com', filter: '"IsInitial":true' },
+          {filterName: 'Exclude onmicrosoft.com', filter: '"IsInitial":false' },
         ],
         path: `/api/DomainAnalyser_List`,
         params: { tenantFilter: currentTenant.defaultDomainName },
